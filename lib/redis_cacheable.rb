7c7afb4d9f89ba13237d671ff9b8c3c1c118c9ed
joker1007 <kakyoin.hierophant@gmail.com>
2013-11-07 18:31:50 +0900

    Add yard style comments

--- lib/redis_cacheable.rb
+++ lib/redis_cacheable.rb
@@ -6,15 +6,44 @@ require 'redis_cacheable/version'
 require 'redis_cacheable/configuration'
 require 'redis_cacheable/connectable'
 
+# Usage:
+#
+# class CacheableObject
+#   include RedisCacheable
+#
+#   attr_reader :id, :name
+#
+#   redis_key :id
+#   redis_attrs :id, :name
+#
+#   def initialize(attrs)
+#     @id = attrs[:id]
+#     @name = attrs[:name]
+#   end
+# end
+#
+# CacheableObject.new(id: 1, name: "object").cache_to_redis
+# CacheableObject.find_from_redis(1) # => {"id" => 1, "name" => "target"}
 module RedisCacheable
   extend ActiveSupport::Concern
   include Connectable
 
   module ClassMethods
+
+    # @param [Symbol || Proc] key used by redis.set method
+    # @example If Symbol, param is method name
+    #   redis_key :uuid
+    # @example If Proc
+    #   redis_key ->(user) {"#{user.id}_{user.email}"}
     def redis_key(key)
       @__redis_cache_key__ = key
     end
 
+    # @param [Array<Symbol> || Proc] attributes
+    # @example If Symbols, param is method name
+    #   redis_attrs :uuid, :name, :email
+    # @example If Proc, proc must return JSON serializable object
+    #   redis_attrs ->(user) { {id: user.id, name: user.name, email: user.email} }
     def redis_attrs(*attributes)
       if attributes.first.is_a?(Proc)
         @__redis_cache_attrs__ = attributes.first
@@ -23,6 +52,8 @@ module RedisCacheable
       end
     end
 
+    # @param [Object] key JSON
+    # @return [String || Number || Array || Hash]
     def find_from_redis(key)
       redis do |conn|
         json = conn.get(key)
--- lib/redis_cacheable/active_record.rb
+++ lib/redis_cacheable/active_record.rb
@@ -1,17 +1,22 @@
 require 'redis_cacheable'
 
+# Specialized for ActiveRecord
 module RedisCacheable
   module ActiveRecord
     extend ActiveSupport::Concern
     include ::RedisCacheable
 
     module ClassMethods
+      # @param [Object] key
+      # @return [ActiveRecord::Base] ActiveRecord instantiated object
       def find_from_redis(key)
         if data = super(key)
           instantiate(data)
         end
       end
 
+      # cache all persisted records
+      # @return [void]
       def cache_all
         find_each do |record|
           record.cache_to_redis
